085db22d873f4687f878c3846804b065ecfab438
Martin Suchan <martin.suchan@outlook.com>
2017-01-29 13:57:05 +0100

    Cleanup in folder structure. Removed obsolete Band API dumps.

--- Source/ApiPeek.Compare.App.Console/Program.cs
+++ Source/ApiPeek.Compare.App.Console/Program.cs
@@ -7,62 +7,34 @@ namespace ApiPeek.Compare.App
     {
         static void Main(string[] args)
         {
-            //string path1 = "win10.15007";
-            //string path2 = "win10.15007";
+            string folder = "api.desktop";
+            string path1 = "win10.15007";
+            string path2 = "win10.15014";
 
-            string path1 = "wp10.15007";
-            string path2 = "wp10.15007";
+            //string folder = "api.mobile";
+            //string path1 = "wp10.15007";
+            //string path2 = "wp10.15007";
 
-            MergeAndCompare(false, path1, path2);
-            MergeAndCompare(true, path1, path2);
-
-            //Console.ReadKey();
+            MergeAndCompare(false, folder, path1, folder, path2);
+            MergeAndCompare(true, folder, path1, folder, path2);
         }
 
-        private static void SortAndCompare(string folder1, string folder2, params string[] fileNames)
+        private static void MergeAndCompare(bool detailed, string folder1, string path1, string folder2, string path2)
         {
-            if (fileNames == null || fileNames.Length == 0)
-            {
-                string[] fileNamesOld = Directory.GetFiles($"api\\{folder1}")
-                    .Where(f => f.EndsWith(".json"))
-                    .Select(f => f.Split('\\').Last())
-                    .ToArray();
-                string[] fileNamesNew = Directory.GetFiles($"api\\{folder2}")
-                    .Where(f => f.EndsWith(".json"))
-                    .Select(f => f.Split('\\').Last())
-                    .ToArray();
-                fileNames = fileNamesOld.Concat(fileNamesNew).Distinct().ToArray();
-            }
-
-            foreach (string fileName in fileNames)
-            {
-                string path1 = $"api\\{folder1}\\{fileName}";
-                string path2 = $"api\\{folder2}\\{fileName}";
-                string pathDiff = $"api\\{folder2}\\diff-{fileName}.txt";
-
-                ApiComparerTxt.Compare(path1, path2, pathDiff);
-            }
-        }
-
-        private static void MergeAndCompare(bool detailed, string folder1, string folder2, params string[] fileNames)
-        {
-            if (fileNames == null || fileNames.Length == 0)
-            {
-                string[] fileNamesOld = Directory.GetFiles($"api\\{folder1}")
-                    .Where(f => f.EndsWith(".json"))
-                    .Select(f => f.Split('\\').Last())
-                    .ToArray();
-                string[] fileNamesNew = Directory.GetFiles($"api\\{folder2}")
-                    .Where(f => f.EndsWith(".json"))
-                    .Select(f => f.Split('\\').Last())
-                    .ToArray();
-                fileNames = fileNamesOld.Concat(fileNamesNew).Distinct().ToArray();
-            }
+            string[] fileNamesOld = Directory.GetFiles($"{folder1}\\{path1}")
+                .Where(f => f.EndsWith(".json"))
+                .Select(f => f.Split('\\').Last())
+                .ToArray();
+            string[] fileNamesNew = Directory.GetFiles($"{folder2}\\{path2}")
+                .Where(f => f.EndsWith(".json"))
+                .Select(f => f.Split('\\').Last())
+                .ToArray();
+            string[] fileNames = fileNamesOld.Concat(fileNamesNew).Distinct().ToArray();
 
             ApiComparerHtml.DetailedDetailLog = detailed;
-            string[] folder1Files = fileNames.Select(f => $"api\\{folder1}\\{f}").ToArray();
-            string[] folder2Files = fileNames.Select(f => $"api\\{folder2}\\{f}").ToArray();
-            string fileName = $"{folder1}.to.{folder2}.{(ApiComparerHtml.DetailedDetailLog ? "full" : "")}diff";
+            string[] folder1Files = fileNames.Select(f => $"{folder1}\\{path1}\\{f}").ToArray();
+            string[] folder2Files = fileNames.Select(f => $"{folder2}\\{path2}\\{f}").ToArray();
+            string fileName = $"{path1}.to.{path2}.{(ApiComparerHtml.DetailedDetailLog ? "full" : "")}diff";
             string pathDiff = $"html\\{fileName}.html";
             ApiComparerHtml.Compare(folder1Files, folder2Files, pathDiff, fileName);
         }
