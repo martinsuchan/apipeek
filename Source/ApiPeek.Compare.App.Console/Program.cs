2b6bf476509dac31aad0d44dea9eaee6fa5ec8ab
Martin Suchan <martin.suchan@outlook.com>
2017-05-11 23:34:37 +0200

    Added API dump from Windows 10 Desktop build 16193.

--- Source/ApiPeek.Compare.App.Console/Program.cs
+++ Source/ApiPeek.Compare.App.Console/Program.cs
@@ -8,8 +8,8 @@ namespace ApiPeek.Compare.App
         static void Main(string[] args)
         {
             string folder = "api.desktop";
-            string path1 = "win10.15063";
-            string path2 = "win10.15063";
+            string path1 = "win10.16193";
+            string path2 = "win10.16193";
 
             //string folder = "api.mobile";
             //string path1 = "wp10.15063";
