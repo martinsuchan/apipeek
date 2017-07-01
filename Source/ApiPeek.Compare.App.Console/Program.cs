40144a30764f4daa9921b1db00be75a8530990a2
Martin Suchan <martin.suchan@outlook.com>
2017-07-01 13:16:29 +0200

    Added API dump from Windows 10 Desktop build 16232.

--- Source/ApiPeek.Compare.App.Console/Program.cs
+++ Source/ApiPeek.Compare.App.Console/Program.cs
@@ -9,7 +9,7 @@ namespace ApiPeek.Compare.App
         {
             string folder = "api.desktop";
             string path1 = "win10.16226";
-            string path2 = "win10.16226";
+            string path2 = "win10.16232";
 
             //string folder = "api.mobile";
             //string path1 = "wp10.15063";
@@ -17,9 +17,13 @@ namespace ApiPeek.Compare.App
 
             MergeAndCompare(false, folder, path1, folder, path2);
             MergeAndCompare(true, folder, path1, folder, path2);
+
+            string path1703 = "win10.15063";
+            MergeAndCompare(false, folder, path1703, folder, path2, "win10.1703.to.win10.1709.diff");
+            MergeAndCompare(true, folder, path1703, folder, path2, "win10.1703.to.win10.1709.fulldiff");
         }
 
-        private static void MergeAndCompare(bool detailed, string folder1, string path1, string folder2, string path2)
+        private static void MergeAndCompare(bool detailed, string folder1, string path1, string folder2, string path2, string fileName = null)
         {
             string[] fileNamesOld = Directory.GetFiles($"{folder1}\\{path1}")
                 .Where(f => f.EndsWith(".json"))
@@ -34,7 +38,10 @@ namespace ApiPeek.Compare.App
             ApiComparerHtml.DetailedDetailLog = detailed;
             string[] folder1Files = fileNames.Select(f => $"{folder1}\\{path1}\\{f}").ToArray();
             string[] folder2Files = fileNames.Select(f => $"{folder2}\\{path2}\\{f}").ToArray();
-            string fileName = $"{path1}.to.{path2}.{(ApiComparerHtml.DetailedDetailLog ? "full" : "")}diff";
+            if (fileName == null)
+            {
+                fileName = $"{path1}.to.{path2}.{(ApiComparerHtml.DetailedDetailLog ? "full" : "")}diff";
+            }
             string pathDiff = $"html\\{fileName}.html";
             ApiComparerHtml.Compare(folder1Files, folder2Files, pathDiff, fileName);
         }
