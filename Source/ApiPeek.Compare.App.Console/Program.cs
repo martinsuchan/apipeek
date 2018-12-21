c720b1b05dd84a7b78751ec2f5b534cf014b51c8
Martin Suchan <martin.suchan@outlook.com>
2018-12-21 20:45:00 +0100

    Added API dump from Windows 10 Desktop build 18305.

--- Source/ApiPeek.Compare.App.Console/Program.cs
+++ Source/ApiPeek.Compare.App.Console/Program.cs
@@ -12,8 +12,8 @@ namespace ApiPeek.Compare.App
         static void Main(string[] args)
         {
             string folder = "api.desktop";
-            string path1 = "win10.18282";
-            string path2 = "win10.18290";
+            string path1 = "win10.18305";
+            string path2 = "win10.18305";
 
             ExtractFiles(folder, path1);
             ExtractFiles(folder, path2);
