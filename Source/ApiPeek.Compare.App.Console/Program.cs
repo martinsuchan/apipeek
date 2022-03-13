dee42d1c01d185083d5cb4ed0ffb5caa0ac02d3f
Martin Suchan <martin.suchan@outlook.com>
2022-03-13 11:08:55 +0100

    Added API dump from Windows 11 Desktop build 22572.

--- Source/ApiPeek.Compare.App.Console/Program.cs
+++ Source/ApiPeek.Compare.App.Console/Program.cs
@@ -5,71 +5,70 @@ using System.IO.Compression;
 using System.Linq;
 using System.Text;
 
-namespace ApiPeek.Compare.App
+namespace ApiPeek.Compare.App;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string folder = "api.desktop";
+        string path1 = "win11.22572";
+        string path2 = "win11.22572";
+
+        ExtractFiles(folder, path1);
+        ExtractFiles(folder, path2);
+        MergeAndCompare(false, folder, path1, folder, path2);
+        MergeAndCompare(true, folder, path1, folder, path2);
+
+        string path11 = "win11.22000";
+        ExtractFiles(folder, path11);
+        MergeAndCompare(false, folder, path11, folder, path2, "win11.to.win11.2022.diff");
+        MergeAndCompare(true,  folder, path11, folder, path2, "win11.to.win11.2022.fulldiff");
+    }
+
+    private static void ExtractFiles(string folder1, string path1)
+    {
+        string dir1Path = $"{folder1}\\{path1}";
+        string zip1Path = $"{dir1Path}.zip";
+        Debug.Assert(File.Exists(zip1Path));
+
+        if (Directory.Exists(dir1Path))
         {
-            string folder = "api.desktop";
-            string path1 = "win11.22463";
-            string path2 = "win11.22509";
-
-            ExtractFiles(folder, path1);
-            ExtractFiles(folder, path2);
-            MergeAndCompare(false, folder, path1, folder, path2);
-            MergeAndCompare(true, folder, path1, folder, path2);
-
-            string path11 = "win11.22000";
-            ExtractFiles(folder, path11);
-            MergeAndCompare(false, folder, path11, folder, path2, "win11.to.win11.2022.diff");
-            MergeAndCompare(true,  folder, path11, folder, path2, "win11.to.win11.2022.fulldiff");
+            Directory.Delete(dir1Path, true);
         }
+        ZipFile.ExtractToDirectory(zip1Path, dir1Path);
+    }
 
-        private static void ExtractFiles(string folder1, string path1)
+    private static void ZipAll(string folder1)
+    {
+        string[] dirs = Directory.GetDirectories(folder1);
+        foreach (string dir in dirs)
         {
-            string dir1Path = $"{folder1}\\{path1}";
-            string zip1Path = $"{dir1Path}.zip";
-            Debug.Assert(File.Exists(zip1Path));
-
-            if (Directory.Exists(dir1Path))
-            {
-                Directory.Delete(dir1Path, true);
-            }
-            ZipFile.ExtractToDirectory(zip1Path, dir1Path);
+            string dirname = dir.Split(new[] { "\\" }, StringSplitOptions.RemoveEmptyEntries)[1];
+            ZipFile.CreateFromDirectory(dir, $"{dirname}.zip", CompressionLevel.Optimal, false, Encoding.UTF8);
         }
+    }
 
-        private static void ZipAll(string folder1)
+    private static void MergeAndCompare(bool detailed, string folder1, string path1, string folder2, string path2, string fileName = null)
+    {
+        string[] fileNamesOld = Directory.GetFiles($"{folder1}\\{path1}")
+            .Where(f => f.EndsWith(".json"))
+            .Select(f => f.Split('\\').Last())
+            .ToArray();
+        string[] fileNamesNew = Directory.GetFiles($"{folder2}\\{path2}")
+            .Where(f => f.EndsWith(".json"))
+            .Select(f => f.Split('\\').Last())
+            .ToArray();
+        string[] fileNames = fileNamesOld.Concat(fileNamesNew).Distinct().ToArray();
+
+        ApiComparerHtml.DetailedDetailLog = detailed;
+        string[] folder1Files = fileNames.Select(f => $"{folder1}\\{path1}\\{f}").ToArray();
+        string[] folder2Files = fileNames.Select(f => $"{folder2}\\{path2}\\{f}").ToArray();
+        if (fileName == null)
         {
-            string[] dirs = Directory.GetDirectories(folder1);
-            foreach (string dir in dirs)
-            {
-                string dirname = dir.Split(new[] { "\\" }, StringSplitOptions.RemoveEmptyEntries)[1];
-                ZipFile.CreateFromDirectory(dir, $"{dirname}.zip", CompressionLevel.Optimal, false, Encoding.UTF8);
-            }
+            fileName = $"{path1}.to.{path2}.{(ApiComparerHtml.DetailedDetailLog ? "full" : "")}diff";
         }
-
-        private static void MergeAndCompare(bool detailed, string folder1, string path1, string folder2, string path2, string fileName = null)
-        {
-            string[] fileNamesOld = Directory.GetFiles($"{folder1}\\{path1}")
-                .Where(f => f.EndsWith(".json"))
-                .Select(f => f.Split('\\').Last())
-                .ToArray();
-            string[] fileNamesNew = Directory.GetFiles($"{folder2}\\{path2}")
-                .Where(f => f.EndsWith(".json"))
-                .Select(f => f.Split('\\').Last())
-                .ToArray();
-            string[] fileNames = fileNamesOld.Concat(fileNamesNew).Distinct().ToArray();
-
-            ApiComparerHtml.DetailedDetailLog = detailed;
-            string[] folder1Files = fileNames.Select(f => $"{folder1}\\{path1}\\{f}").ToArray();
-            string[] folder2Files = fileNames.Select(f => $"{folder2}\\{path2}\\{f}").ToArray();
-            if (fileName == null)
-            {
-                fileName = $"{path1}.to.{path2}.{(ApiComparerHtml.DetailedDetailLog ? "full" : "")}diff";
-            }
-            string pathDiff = $"html\\{fileName}.html";
-            ApiComparerHtml.Compare(folder1Files, folder2Files, pathDiff, fileName);
-        }
+        string pathDiff = $"html\\{fileName}.html";
+        ApiComparerHtml.Compare(folder1Files, folder2Files, pathDiff, fileName);
     }
 }
