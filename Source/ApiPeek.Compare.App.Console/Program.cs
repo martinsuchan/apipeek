6e43c944932e55db7ebfa8a73449a313ba4d8f00
Martin Suchan <martin.suchan@outlook.com>
2024-07-11 08:13:52 +0200

    Added API dump from Windows 11 Desktop build 26252.

--- Source/ApiPeek.Compare.App.Console/Program.cs
+++ Source/ApiPeek.Compare.App.Console/Program.cs
@@ -5,18 +5,18 @@ using ApiPeek.Compare.App;
 
 
 string folder = "api.desktop";
-string path1 = "win11.25987";
-string path2 = "win11.25987";
+string path1 = "win11.26252";
+string path2 = "win11.26252";
 
 ExtractFiles(folder, path1);
 ExtractFiles(folder, path2);
 MergeAndCompare(false, folder, path1, folder, path2);
 MergeAndCompare(true, folder, path1, folder, path2);
 
-string path11 = "win11.22631";
+string path11 = "win11.26090"; // TODO use 26100
 ExtractFiles(folder, path11);
-MergeAndCompare(false, folder, path11, folder, path2, "win11.23H2.to.win11.24H2.diff");
-MergeAndCompare(true,  folder, path11, folder, path2, "win11.23H2.to.win11.24H2.fulldiff");
+MergeAndCompare(false, folder, path11, folder, path2, "win11.24H2.to.win11.25H2.diff");
+MergeAndCompare(true,  folder, path11, folder, path2, "win11.24H2.to.win11.25H2.fulldiff");
 
 
 static void ExtractFiles(string folder1, string path1)
